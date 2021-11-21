1eb8fba6b5fe706ed563151b88c83bb8e00752ca
kamrunnaharruma <knahar12bd@gmail.com>
2021-11-21 16:04:17 +0600

    header component added

--- src/App.js
+++ src/App.js
@@ -4,6 +4,11 @@ function App() {
   return (
     <div className="App">
       <h1>facebook clone banai</h1>
+      {/* Header Component */}
+      {/* App Body */}
+      {/* Side Bar */}
+      {/* Feed */}
+      {/* Widgets */}
     </div>
   );
 }
